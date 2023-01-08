6c7dcca39c52a16100299566ee6e8cefc27f1421
Christian Unigarro <cunigarrod@gmail.com>
2023-01-08 17:46:18 -0500

    Task list component improved with undefined type.

--- src/app/features/todo-list/components/task-list/task-list.component.ts
+++ src/app/features/todo-list/components/task-list/task-list.component.ts
@@ -1,10 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { Task } from '../../types/task';
 
 @Component({
   selector: 'task-list',
-  templateUrl: './task-list.component.html'
+  templateUrl: './task-list.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TaskListComponent implements OnInit {
+  @Input() taskList: Task[] | undefined;
+
   constructor() { }
 
   ngOnInit(): void { }
--- src/app/features/todo-list/todo-list.module.ts
+++ src/app/features/todo-list/todo-list.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { TodoListRoutingModule } from './todo-list-routing.module';
 import { TodoListComponent } from './pages/todo-list/todo-list.component';
 import { TaskFormComponent } from './components/task-form/task-form.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
 
 @NgModule({
   declarations: [
     TodoListComponent,
+    TaskListComponent,
     TaskFormComponent
   ],
   imports: [
